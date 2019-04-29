0c2fd97ffef16575a40fe2c4000f1c409117d43d
Yotam Vaknin <tomirendo@gmail.com>
2019-04-29 19:44:53 +0200

    deleted public

--- public/styles-7d2417aeb0aae09d6872.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{106:function(n,o,w){},107:function(n,o,w){},147:function(n,o,w){}}]);
-//# sourceMappingURL=styles-7d2417aeb0aae09d6872.js.map
